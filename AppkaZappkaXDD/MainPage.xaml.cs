98d03f556871bfe068cb65b4112c85104e7dab10
metinek <uT_3612@zsp3zamosc.pl>
2024-01-02 20:26:51 +0100

    z

--- AppkaZappkaXDD/MainPage.xaml.cs
+++ AppkaZappkaXDD/MainPage.xaml.cs
@@ -2,26 +2,35 @@
     public partial class MainPage : ContentPage {
         Random rnd = new Random();
         string[] karty = {"ace_diamond", "ace_heart", "ace_spade", "ace_trefl", "joker_black", "joker_red"};
-        int[] wylosowane;
-        int k;
-        public MainPage() {
+        HashSet<int> wylosowane = new HashSet<int>();
+        int k=0;
+        public MainPage() { 
             InitializeComponent();
         }
 
         private void OnLosujClicked(object sender, EventArgs e) {
-            if (wylosowane.Length-2<karty.Length) {
-                while (!wylosowane.Contains(k))
-                    k = rnd.Next(6);
+            if (wylosowane.Count < karty.Length) {
+                k = rnd.Next(karty.Length);
+                while (wylosowane.Contains(k))
+                    k = rnd.Next(karty.Length);
 
-
-                wylosowane[wylosowane.Length-1] = k;
-                cardImg.Source = karty[k]+".png";
-
+                wylosowane.Add(k);
+                cardImg.Source = karty[k] + ".png";
 
             }
-            else
+            else {
                 cardImg.Source = "none.png";
-
+                losujBtn.IsEnabled = false;
+            }
         }
+
+        private void OnResetClicked(object sender, EventArgs e) {
+            wylosowane.Clear();
+            k = 0;
+            losujBtn.IsEnabled = true;
+        }
+
+
+
     }
 }
