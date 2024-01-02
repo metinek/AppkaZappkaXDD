5c8609508bc4284c0d6eab3004c0682b2e4339c3
metinek <ut_3612@zsp3zamosc.pl>
2024-01-02 13:05:58 +0100

    Dodaj pliki projektów.

---
+++ AppkaZappkaXDD/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppkaZappkaXDD
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppkaZappkaXDD/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace AppkaZappkaXDD {
+    public partial class MainPage : ContentPage {
+        Random rnd = new Random();
+        string[] karty = {"ace_diamond", "ace_heart", "ace_spade", "ace_trefl", "joker_black", "joker_red"};
+        int[] wylosowane;
+        int k;
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void OnLosujClicked(object sender, EventArgs e) {
+            if (wylosowane.Length-2<karty.Length) {
+                while (!wylosowane.Contains(k))
+                    k = rnd.Next(6);
+
+
+                wylosowane[wylosowane.Length-1] = k;
+                cardImg.Source = karty[k]+".png";
+
+
+            }
+            else
+                cardImg.Source = "none.png";
+
+        }
+    }
+}
